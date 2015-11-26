24b2ffcccb3dbe16a698dacdc35ba670accb3fd7
Jacob Burden <Jacob.JW.Burden@gmail.com>
2015-11-26 12:37:16 -0500

    checks if a user exist before writing to the db.

--- routes/users/signup.js
+++ routes/users/signup.js
@@ -5,9 +5,16 @@ module.exports = (req, res, db, pass, params) => {
     params.password = undefined
     const user = Object.assign({}, params, {'salt': salt}, {'hash': hash})
 
-    db.put(`users\x00${params.username}`, user, err => {
-      if (err) console.error(err)
-      res.end(`${params.username} has signed up!\n`)
+    db.get(`users\x00${params.username}`, (err, _) => {
+      if (err) {
+        if (err.notFound) {
+          db.put(`users\x00${params.username}`, user, err => {
+            if (err) console.error(err)
+            res.end(`${params.username} has signed up!\n`)
+          })
+        } else console.error(err)
+      } else res.end(`${params.username} already exist, please try again.\n`)
     })
   })
 }
+
