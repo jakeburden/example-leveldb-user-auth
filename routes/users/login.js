4bba961094860641c5d9637c008121fe85900c2e
Jacob Burden <Jacob.JW.Burden@gmail.com>
2015-11-26 12:21:56 -0500

    attempting to login with an unknown username will result in the message 'Incorrect username or password'.

--- routes/users/login.js
+++ routes/users/login.js
@@ -2,12 +2,15 @@ module.exports = (req, res, db, pass, params) => {
   db.get(`users\x00${params.username}`, (err, user) => {
     if (err) console.error(err)
 
-    pass.hash(params.password, user.salt, (err, hash) => {
-      if (err) console.error(err)
+    if (user) {
+      pass.hash(params.password, user.salt, (err, hash) => {
+        if (err) console.error(err)
 
-      user.hash === hash
-       ? res.end(`Auth! Hello ${user.username}\n`)
-       : res.end('Incorrect username or password\n')
-    })
+        user.hash === hash
+          ? res.end(`Auth! Hello ${user.username}\n`)
+          : res.end('Incorrect username or password\n')
+      })
+    } else res.end('Incorrect username or password\n')
   })
 }
+
