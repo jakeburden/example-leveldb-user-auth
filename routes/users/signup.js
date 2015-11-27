c337010edf47070b12dcb06de09e9dcf5b945c3f
Jacob Burden <Jacob.JW.Burden@gmail.com>
2015-11-26 21:39:51 -0500

    set minimum username and password lengths

--- routes/users/signup.js
+++ routes/users/signup.js
@@ -1,4 +1,7 @@
 module.exports = (req, res, db, pass, params) => {
+  const validUserName = params.username.length >= 1
+  const validPassword = params.password.length >= 6
+
   pass.hash(params.password, (err, salt, hash) => {
     if (err) console.error(err)
 
@@ -8,6 +11,14 @@ module.exports = (req, res, db, pass, params) => {
     db.get(`users\x00${params.username}`, (err, _) => {
       if (err) {
         if (err.notFound) {
+
+          if (!validUserName) {
+            return res.end('Usernames need to have at least 1 character.\n')
+          }
+          if (!validPassword) {
+            return res.end('Passwords need to have at least 6 characters.\n')
+          }
+
           db.put(`users\x00${params.username}`, user, err => {
             if (err) console.error(err)
             res.end(`${params.username} has signed up!\n`)
