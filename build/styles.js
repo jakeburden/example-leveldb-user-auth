f01c3025a17539d043f5d4bacad38ecd5bbd73df
Jacob Burden <jacob.burden@qscend.com>
2015-12-11 15:23:16 -0500

    fixed build step issues.  need to implement cache breaking

--- build/styles.js
+++ build/styles.js
@@ -8,33 +8,25 @@ const uncss = require('uncss')
 
 const css = fs.readFileSync('src/styles/index.css', 'utf8')
 
-const index = fs.readFileSync('static/index.html', 'utf8')
-const signup = fs.readFileSync('templates/signup.html', 'utf8')
-const login = fs.readFileSync('templates/login.html', 'utf8')
-
-const opts = {
-  htmlroot: 'public',
-  stylesheets: ['styles/bundle.css']
-}
+const htmls = ['static/index.html',
+               'templates/signup.html',
+               'templates/login.html']
 
 postcss([atImport, autoprefixer])
-  .process(css, {
-    from: 'src/styles/index.css',
-    to: 'public/styles/bundle.css'
-  })
+  .process(css)
   .then(result => {
     fs.writeFileSync('public/styles/bundle.css', result.css)
-    uncss([index, login, signup], opts, function (err, output) {
+    const opts = {
+      stylesheets: ['styles/bundle.css'],
+      csspath: '../public'
+    }
+    uncss(htmls, opts, (err, output) => {
       if (err) console.error(err)
       fs.writeFileSync('public/styles/bundle.css', output)
-
       postcss([cssnano])
-        .process(output, {
-          from: 'public/styles/bundle.css',
-          to: 'public/styles/bundles.css'
-        })
+        .process(output)
         .then(result => {
-          fs.writeFileSync('public/styles/bundle.css', result)
+          fs.writeFileSync('public/styles/bundle.css', result.css)
         })
     })
   })
